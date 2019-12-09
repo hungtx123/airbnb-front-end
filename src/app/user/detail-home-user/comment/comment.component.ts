948407cc6be680e196143d315dcc4041404fd0f9
phongtiger <phongken94@gmail.com>
2019-12-09 10:34:09 +0700

    sua comment v2

--- src/app/user/detail-home-user/comment/comment.component.ts
+++ src/app/user/detail-home-user/comment/comment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {UserService} from '../../../service/user.service';
 import {ActivatedRoute} from '@angular/router';
+import {HostService} from '../../../service/host.service';
 
 @Component({
   selector: 'app-comment',
@@ -10,29 +11,38 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class CommentComponent implements OnInit {
   commentForm: FormGroup;
+  comments: string;
 
   constructor(private userService: UserService,
+              private hostService: HostService,
               private fb: FormBuilder,
-              private route: ActivatedRoute,) {}
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.commentForm = this.fb.group({
-      house:'',
+      house: '',
       comment: '',
       rate: ''
-  });
+    });
     const id = +this.route.snapshot.paramMap.get('id');
   }
 
   comment() {
     if (this.commentForm.valid) {
       const id = +this.route.snapshot.paramMap.get('id');
-      this.commentForm.patchValue( {house: {id: id}});
-      const{value} = this.commentForm;
+      this.commentForm.patchValue({house: {id: id}});
+      const {value} = this.commentForm;
       this.userService.comment(value).subscribe(
         next => {
-          console.log('comment thanh cong')
-        }, error => {console.log('comment k thanh cong')});
+          this.hostService.getListComment(id).subscribe(next2 => {
+              this.comments = next2;
+              console.log('comment thanh cong');
+            }, error => {
+              console.log('comment k thanh cong');
+            }
+          );
+        });
     }
   }
 }
